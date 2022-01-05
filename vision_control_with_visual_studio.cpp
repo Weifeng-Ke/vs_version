ea591d96f9d1bdaa63e3f8be795aab0cd3bc2060
weifeng <kenweifeng01@outlook.com>
2022-01-04 23:27:35 -0800

    this is the master commit

--- vision_control_with_visual_studio.cpp
+++ vision_control_with_visual_studio.cpp
@@ -7,6 +7,7 @@ int main()
 {
     //std::cout << "Hello World!\n";
     cout << "hello there, this is version control with visual studio";
+    cout << "This is the version edited under slave branch";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
